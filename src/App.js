d1d64274b1f2846dca2dce7ee8c3fb9c5e2b9db3
Sergey Volkov <serzhvolkov03@gmail.com>
2025-03-25 19:50:58 +0300

    feat: add react routes, header and main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import "../Header/Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>My App</h1>
+      <p>
+        Contains information about users, their posts and comments on these
+        posts.
+      </p>
+    </header>
+  );
+}
